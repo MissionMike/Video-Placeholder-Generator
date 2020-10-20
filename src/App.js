3f75fe25762078f97560d8738d18a113400c47d3
Michael Dinerstein <mdinerstein@rrfedu.com>
2020-10-19 18:05:06 -0700

    Added Prettier config, code refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from "react";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-	return (
-		<div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-			<div className="ml-6 pt-1">
-				<h1 className="mb-4 text-2xl text-blue-700 leading-tight">
-					Video Placeholder Generator
-				</h1>
-				<form class="block bg-white w-full shadow-md rounded px-8 pt-6 pb-8 mb-4">
-				<div class="mb-4 w-full">
-				  <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-					Video URL (YouTube or Vimeo)
-				  </label>
-				  <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="video-url" type="text" placeholder="https://www.youtube.com/watch?v=dckX1V0Fi3c" />
-				</div>
-				</form>
-				<p className="text-base text-gray-700 leading-normal">Building apps together</p>
-			</div>
-		</div>
-	);
-}
+import Header from "./components/Header";
+import VideoPlaceholder from "./components/VideoPlaceholder";
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    videoUrl:
+      "https://tools.missionmike.dev/thumbnail-generator/fe1gcKVcX6o.jpg?save=false&play_button_opacity=0.5",
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="flex content-around flex-wrap bg-gray-200 h-48">
+          <div className="lg:w-1/3"></div>
+          <div className="w-full lg:w-1/3 p-2">
+            <label for="video-url" className="text-bold mb-2 block pl-1">
+              Video URL (YouTube, Vimeo):
+            </label>
+            <input
+              type="url"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value=""
+              placeholder="https://www.youtube.com/watch?v=dckX1V0Fi3c"
+              id="video-url"
+              name="video-url"
+            />
+          </div>
+        </div>
+        <div className="lg:w-1/3"></div>
+        <VideoPlaceholder />
+      </>
+    );
+  }
+}
