58991da16962fd969c642fc88182b22bdff1179d
Michael Dinerstein <michael@dtweb.design>
2020-10-31 16:14:47 -0700

    Added default thumbnails folder

--- src/defaults.js
+++ src/defaults.js
@@ -1,9 +1,16 @@
+/**
+ * Change these defaults to your own.
+ * 
+ * After setting up the server-side support, set baseUrl to your own.
+ * 
+ * Feel free to use the default play button graphic listed below, however continued
+ * support/hosting is not guaranteed :)
+ */
 module.exports = {
-  baseUrl: "https://tools.missionmike.dev/video-thumbnail-generator/thumbnails/",
+  baseUrl: "https://www.missionmike.dev/video-thumbnail-generator/thumbnails/",
   thumbnailWidth: 600,
   playButtonWidth: 80,
-  playButtonUrl: "https://tools.missionmike.dev/video-thumbnail-generator/assets/youtube_play.png",
+  playButtonUrl: "https://www.missionmike.dev/video-thumbnail-generator/assets/youtube_play.png",
   videoId: "7NOSDKb0HlU",
   playButtonOpacity: 80,
-  copySuccess: false,
 };
