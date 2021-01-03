f20ab0eac894bc29dee31b14d1b366c8b5b3bccb
Michael Dinerstein <michael@dtweb.design>
2021-01-03 13:08:33 -0800

    Moved analyticsSample to analytics.js and added support for environment vars

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,6 @@
 /**
- * This is a sample analytics.js file. If you'd like to include your own analytics,
- * you can plug in your own tracking codes below, and copy/rename this file to "analytics.js"
+ * If you'd like to include your own analytics,
+ * you can plug in your own tracking codes into .env for use below.
  *
  * The following dependencies are included in package.json; please remove any that you don't
  * want to use:
@@ -15,26 +15,33 @@ import LogRocket from "logrocket";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
+require("dotenv").config();
+
 /**
  * Set the "environment" variable for Sentry, and conditional loading of Google and LogRocket
  */
 const environment = window && window.location.hostname.includes("localhost") ? "production" : "development";
 
-Sentry.init({
-  dsn: "https://your.sentry.io.url.here",
-  integrations: [new Integrations.BrowserTracing()],
-  environment: environment,
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-});
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    environment: environment,
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
 
 /**
  * On production only, initialize LogRocket and Google Analytics
  */
 if (environment === "production") {
-  LogRocket.init("abcdef/your-logrocket-project-url");
-
-  ReactGA.initialize("UA-0000000-1");
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  if (process.env.LOGROCKET_PROJECT) {
+    LogRocket.init(process.env.LOGROCKET_PROJECT);
+  }
+  if (process.env.GA_TRACKING) {
+    ReactGA.initialize(process.env.GA_TRACKING);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
 }
--- src/defaults.js
+++ src/defaults.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
+
 /**
  * Change these defaults to your own.
- * 
+ *
  * After setting up the server-side support, set baseUrl to your own.
- * 
+ *
  * Feel free to use the default play button graphic listed below, however continued
  * support/hosting is not guaranteed :)
  */
 module.exports = {
-  serverUrl: "https://api.missionmike.dev/video-placeholder-generator/",
+  serverUrl: process.env.SERVER_URL || "https://api.missionmike.dev/video-placeholder-generator/",
   thumbnailWidth: 600,
   playButtonWidth: 80,
   playButtonUrl: "",
-  videoId: "H79gstz4qZI",
+  videoId: process.env.VIDEO_ID || "H79gstz4qZI",
   playButtonOpacity: 80,
 };
